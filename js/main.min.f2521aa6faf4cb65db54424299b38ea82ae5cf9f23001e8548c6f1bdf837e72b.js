3ab55d9752ab546f3e372b10cea75fda26b7b45d
derkoe <christian.koeberl@gmail.com>
2018-12-23 22:50:33 +0100

    Fixed header, minfied everything

---
+++ js/main.min.f2521aa6faf4cb65db54424299b38ea82ae5cf9f23001e8548c6f1bdf837e72b.js
@@ -0,0 +1,5 @@
+const throttle=(callback,limit)=>{let timeoutHandler=null;return()=>{if(timeoutHandler==null){timeoutHandler=setTimeout(()=>{callback();timeoutHandler=null;},limit);}};};let mobileMenu=document.getElementById('mobile-menu');let mobileMenuVisible=false;const mobileMenuToggle=()=>{if(mobileMenuVisible==false){mobileMenu.style.animationName='bounceInRight';mobileMenu.style.webkitAnimationName='bounceInRight';mobileMenu.style.display='block';mobileMenuVisible=true;}else{mobileMenu.style.animationName='bounceOutRight';mobileMenu.style.webkitAnimationName='bounceOutRight'
+mobileMenuVisible=false;}}
+const showFeaturedImg=()=>{document.getElementById('bg-img').classList.add('show-bg-img');}
+const showContent=()=>{document.getElementById('bg-img').classList.remove('show-bg-img');}
+if(haveHeader==true){document.getElementById('menu-btn').addEventListener('click',mobileMenuToggle);window.addEventListener('scroll',throttle(()=>{if(mobileMenuVisible==true){mobileMenuToggle();}},250));}
